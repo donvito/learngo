612ba3a0e50d7a069ce57420aa0e9ea00dd48d22
Melvin Vivas <melvindave@gmail.com>
2019-11-12 22:30:00 +0800

    added simplest http server

---
+++ http-server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	err := http.ListenAndServe(":6000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
